12b1eef2e62d3b3ddd7cd46b3e820839a59628fb
asheimstablet <asheimstablet@outlook.com>
2026-04-23 18:01:17 +0800

    Array.some,Array.reverse

--- compilerTests/CompileTest/array/reverse/Test001.cs
+++ compilerTests/CompileTest/array/reverse/Test001.cs
@@ -5,10 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace compilerTests.CompileTest.array.some
+namespace compilerTests.CompileTest.array.reverse
 {
 	[TestClass]
-	public sealed class Test019 : CodeTestBase
+	public sealed class Test001 : CodeTestBase
 	{
 		protected override TestCodeProject LoadProject()
 		{
@@ -26,10 +26,22 @@ namespace compilerTests.CompileTest.array.some
 package ns1 
 {
 	import flash.display.Sprite;
+	/**
+	 * ...
+	 * @author 
+	 */
 	public class BaseM extends Sprite
 	{
-		public function BaseM() {}
+		
+		public static const FFF = 6666;
+		protected static const VVV = ""abcd"";
+		public function BaseM() 
+		{
+			
+		}
+		
 	}
+
 }
 
 
@@ -48,6 +60,10 @@ package
 	import ns1.BaseM;
 	
 	[Doc]
+	/**
+	 * ...
+	 * @author 
+	 */
 	public class Main extends BaseM
 	{
 		public var v;
@@ -69,18 +85,23 @@ function assert(mustBeTrue, message = undefined) {
   if (mustBeTrue === true) {
     return;
   }
+
   if (message === undefined) {
     message = 'Expected true but got ' + assert._toString(mustBeTrue);
   }
   throw new Test262Error(message);
 }
 
-assert._toString = function (v:String) { return v; }
+assert._toString = function (v:String) 
+{
+	return v;
+}
 
 assert._isSameValue = function (a, b) {
   if (a === b) {
     return a !== 0 || 1 / a === 1 / b;
   }
+
   return a !== a && b !== b;
 };
 
@@ -91,89 +112,51 @@ assert.sameValue = function (actual, expected, message) {
     }
   } catch (error) {
     throw new Test262Error(message + ' (_isSameValue operation threw) ' + error);
+    return;
   }
+
   if (message === undefined) {
     message = '';
   } else {
     message += ' ';
   }
+
   message += 'Expected SameValue(«' + assert._toString(actual) + '», «' + assert._toString(expected) + '») to be true';
+
   throw new Test262Error(message);
 };
 
-var arr = [1, 2, 3, 4, 5];
-if (arr.some(function(x:int):Boolean { return x > 3; }) !== true) {
-  throw new Test262Error('#1: some with matching element');
+var x = new Array();
+if (x.reverse().length !== 0) {
+  throw new Test262Error('#1: x = new Array(); x.reverse().length !== 0');
 }
 
-arr = [1, 2, 3, 4, 5];
-if (arr.some(function(x:int):Boolean { return x > 10; }) !== false) {
-  throw new Test262Error('#2: some with no matching element');
+x = [1];
+var r1 = x.reverse();
+if (r1[0] !== 1) {
+  throw new Test262Error('#2: x = [1]; r.reverse()[0] !== 1');
 }
 
-arr = [];
-if (arr.some(function(x:int):Boolean { return x > 0; }) !== false) {
-  throw new Test262Error('#3: empty array');
+x = [1, 2];
+var r2 = x.reverse();
+if (r2[0] !== 2 || r2[1] !== 1) {
+  throw new Test262Error('#3: x = [1,2]; r.reverse()');
 }
 
-arr = [1, 2, 3];
-var checked = 0;
-var result = arr.some(function(element:int, index:int, a:Array):Boolean {
-  checked++;
-  return element == 2 && index == 1 && a.length == 3;
-});
-if (result !== true) {
-  throw new Test262Error('#4: callback args');
-}
-if (checked !== 2) {
-  throw new Test262Error('#4: checked count');
+x = [1, 2, 3];
+var r3 = x.reverse();
+if (r3[0] !== 3 || r3[1] !== 2 || r3[2] !== 1) {
+  throw new Test262Error('#4: x = [1,2,3]; r.reverse()');
 }
 
-arr = [0, 1, 2];
-if (arr.some(function(x:int):Boolean { return x > 0; }) !== true) {
-  throw new Test262Error('#5: with falsy 0');
+var arr = [1, 2, 3];
+var reversed = arr.reverse();
+if (reversed !== arr) {
+  throw new Test262Error('#7: arr.reverse() should return same reference');
 }
 
-arr = [-1, -2, -3];
-if (arr.some(function(x:int):Boolean { return x > 0; }) !== false) {
-  throw new Test262Error('#6: all negative');
-}
-
-arr = [undefined, undefined];
-if (arr.some(function(x:*):Boolean { return x !== undefined; }) !== false) {
-  throw new Test262Error('#7: only undefined');
-}
-
-arr = [undefined, 1, undefined];
-result = arr.some(function(x:*):Boolean { return x === 1; });
-if (result !== true) {
-  throw new Test262Error('#8: with undefined and match');
-}
-
-arr = [null, null, 1];
-result = arr.some(function(x:*):Boolean { return x === 1; });
-if (result !== true) {
-  throw new Test262Error('#9: with null');
-}
-
-arr = [1, 2, 3, 4, 5];
-var found = false;
-result = arr.some(function(x:int):Boolean {
-  found = true;
-  return x == 3;
-});
-if (result !== true || found !== true) {
-  throw new Test262Error('#10: early return');
-}
-
-arr = [1, 2, 3];
-var outerVar = 0;
-result = arr.some(function(x:int):Boolean {
-  outerVar = x * 2;
-  return x == 2;
-});
-if (outerVar !== 4) {
-  throw new Test262Error('#11: outer var');
+if (arr[0] !== 3 || arr[1] !== 2 || arr[2] !== 1) {
+  throw new Test262Error('#8: reverse should modify original array');
 }
 
 trace('OK');
